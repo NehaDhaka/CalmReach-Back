ea314c79641cdb8bf5f07bcf384c271bd6368c90
Neha Dhaka <nehadhaka43@gmail.com>
2023-06-14 19:29:45 -0400

    setting up registration

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const cors = require("cors");
+const userRoutes = require("./routes/userRoutes");
+
+const app = express();
+require("dotenv").config();
+
+app.use(cors());
+app.use(express.json());
+app.use("/api/auth", userRoutes);
+
+const server = app.listen(process.env.PORT, () => {
+  console.log(`Server started on Port ${process.env.PORT}`);
+});
